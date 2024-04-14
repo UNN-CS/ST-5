a9137432dfaa137e49dc0bd92594be4f9291723e
MineShadow7 <shad-karagodin@ya.ru>
2024-04-14 22:44:03 +0300

    Check if rename fixes tests search

--- src/test/java/com/st5/SqrtClassTests.java
+++ src/test/java/com/st5/SqrtClassTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static junit.framework.Assert.*;
 
-public class SqrtClassTest {
+public class SqrtClassTests {
     private static final double DELTA = 1e-6;
 
     @Test
