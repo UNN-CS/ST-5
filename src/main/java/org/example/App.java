fedc8266f8e9edb88f33f90ff780871074de33c2
Yurin Andrey <andreyyurin@icloud.com>
2024-04-15 16:53:24 +0300

    Изменил месторасположение файлов

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main( String[] args )
     {
         double val=Double.parseDouble("2.0");
-        Sqrt sqrt=new Sqrt(val);
+        Sqrt sqrt = new Sqrt(val);
         double result=sqrt.calc();
         System.out.println("Sqrt of " + val + " = " + result);
     }
