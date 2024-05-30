a7b008226ccab9f7613256fff94cb52b46062dcb
Marissa696 <mrnsrtv@gmail.com>
2024-05-30 15:34:38 +0300

    fix tests

--- src/test/java/SqrtTests.java
+++ src/test/java/SqrtTests.java
@@ -6,45 +6,57 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SqrtTests {
     @Test
-    public void testSqrtOfZero() {
-        Sqrt sqrt = new Sqrt(0);
-        assertEquals(0.0, sqrt.calc(), 0.0001);
+    public void testSqrtOf2() {
+        Sqrt sqrt = new Sqrt(2.0);
+        double result = sqrt.calc();
+        assertEquals(1.4142135623730951, result, 0.00000001);
     }
 
     @Test
-    public void testBadSquare() {
-        Sqrt sqrt = new Sqrt(5);
-        assertEquals(Math.sqrt(5), sqrt.calc(), 0.000001);
+    public void testSqrtOf4() {
+        Sqrt sqrt = new Sqrt(4.0);
+        double result = sqrt.calc();
+        assertEquals(2.0, result, 0.00000001);
+    }
+
+    @Test
+    public void testSqrtOf9() {
+        Sqrt sqrt = new Sqrt(9.0);
+        double result = sqrt.calc();
+        assertEquals(3.0, result, 0.00000001);
+    }
+
+    @Test
+    public void testSqrtOf16() {
+        Sqrt sqrt = new Sqrt(16.0);
+        double result = sqrt.calc();
+        assertEquals(4.0, result, 0.00000001);
     }
 
     @Test
     public void testSqrtOfNegativeNumber() {
-        Sqrt sqrt = new Sqrt(-12);
-        assertThrows(Throwable.class, sqrt::calc);
+        Sqrt sqrt = new Sqrt(-1.0);
+        assertThrows(IllegalArgumentException.class, () -> sqrt.calc());
     }
 
     @Test
-    public void testSqrtOfMaxValue() {
-        Sqrt sqrt = new Sqrt(Double.MAX_VALUE);
+    public void testSqrtOfZero() {
+        Sqrt sqrt = new Sqrt(0.0);
         double result = sqrt.calc();
-        assertEquals(false, Double.isNaN(result));
+        assertEquals(0.0, result, 0.00000001);
     }
-    
+
+    @Test
+    public void testSqrtOfSmallNumber() {
+        Sqrt sqrt = new Sqrt(0.00000001);
+        double result = sqrt.calc();
+        assertEquals(0.000100000000001e-4, result, 0.00000001);
+    }
+
     @Test
     public void testSqrtOfLargeNumber() {
-        Sqrt sqrt = new Sqrt(1000000);
-        assertEquals(1000.0, sqrt.calc(), 0.000001);
-    }
-
-    @Test
-    public void testSqrtOfFraction() {
-        Sqrt sqrt = new Sqrt(0.16);
-        assertEquals(0.4, sqrt.calc(), 0.000001);
-    }
-
-    @Test
-    public void testNormalSquare() {
-        Sqrt sqrt = new Sqrt(4);
-        assertEquals(2.0, sqrt.calc(), 0.000001);
+        Sqrt sqrt = new Sqrt(999999999.0);
+        double result = sqrt.calc();
+        assertEquals(31622.7766, result, 0.0001);
     }
 }
