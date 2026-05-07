b73d41f5e189c9559504c52ee3127a78f3cae91c
milka <vladolesnitskiy@gmail.com>
2026-05-08 00:24:06 +0300

    fix

--- my-app/src/test/java/com/mycompany/app/SqrtTest.java
+++ my-app/src/test/java/com/mycompany/app/SqrtTest.java
@@ -11,73 +11,73 @@ public class SqrtTest {
 
     @Test
     void averageOfTwoPositiveNumbers() {
-        Sqrt sqrt = new Sqrt(3.0);
-        assertEquals(3.5, sqrt.average(2.0, 5.0), EPS);
+        Sqrt sqrt = new Sqrt(2.0);
+        assertEquals(2.0, sqrt.average(1.0, 3.0), EPS);
     }
 
     @Test
     void averageOfEqualNumbers() {
-        Sqrt sqrt = new Sqrt(3.0);
-        assertEquals(7.0, sqrt.average(7.0, 7.0), EPS);
+        Sqrt sqrt = new Sqrt(2.0);
+        assertEquals(5.0, sqrt.average(5.0, 5.0), EPS);
     }
 
     @Test
     void goodReturnsTrueForExactSquareRoot() {
-        Sqrt sqrt = new Sqrt(9.0);
-        assertTrue(sqrt.good(3.0, 9.0));
+        Sqrt sqrt = new Sqrt(4.0);
+        assertTrue(sqrt.good(2.0, 4.0));
     }
 
     @Test
     void goodReturnsFalseForBadGuess() {
-        Sqrt sqrt = new Sqrt(9.0);
-        assertFalse(sqrt.good(2.0, 9.0));
+        Sqrt sqrt = new Sqrt(4.0);
+        assertFalse(sqrt.good(1.0, 4.0));
     }
 
     @Test
-    void improveChangesGuessForThree() {
-        Sqrt sqrt = new Sqrt(3.0);
-        assertEquals(2.0, sqrt.improve(2.0, 3.0), EPS);
+    void improveChangesGuessForTwo() {
+        Sqrt sqrt = new Sqrt(2.0);
+        assertEquals(1.5, sqrt.improve(1.0, 2.0), EPS);
     }
 
     @Test
-    void iterFindsSqrtOfNine() {
-        Sqrt sqrt = new Sqrt(9.0);
-        assertEquals(3.0, sqrt.iter(1.0, 9.0), EPS);
-    }
-
-    @Test
-    void calcFindsSqrtOfThree() {
-        Sqrt sqrt = new Sqrt(3.0);
-        assertEquals(Math.sqrt(3.0), sqrt.calc(), EPS);
+    void iterFindsSqrtOfFour() {
+        Sqrt sqrt = new Sqrt(4.0);
+        assertEquals(2.0, sqrt.iter(1.0, 4.0), EPS);
     }
 
     @Test
     void calcFindsSqrtOfTwo() {
         Sqrt sqrt = new Sqrt(2.0);
-        assertEquals(1.4142135623730951, sqrt.calc(), EPS);
+        assertEquals(Math.sqrt(2.0), sqrt.calc(), EPS);
     }
 
     @Test
-    void calcFindsSqrtOfTwentyFive() {
-        Sqrt sqrt = new Sqrt(25.0);
-        assertEquals(5.0, sqrt.calc(), EPS);
+    void calcFindsSqrtOfOne() {
+        Sqrt sqrt = new Sqrt(1.0);
+        assertEquals(1.0, sqrt.calc(), EPS);
     }
 
     @Test
-    void calcFindsSqrtOfThirtySix() {
-        Sqrt sqrt = new Sqrt(36.0);
-        assertEquals(6.0, sqrt.calc(), EPS);
+    void calcFindsSqrtOfNine() {
+        Sqrt sqrt = new Sqrt(9.0);
+        assertEquals(3.0, sqrt.calc(), EPS);
+    }
+
+    @Test
+    void calcFindsSqrtOfSixteen() {
+        Sqrt sqrt = new Sqrt(16.0);
+        assertEquals(4.0, sqrt.calc(), EPS);
     }
 
     @Test
     void calcFindsSqrtOfSmallFraction() {
-        Sqrt sqrt = new Sqrt(0.64);
-        assertEquals(0.8, sqrt.calc(), EPS);
+        Sqrt sqrt = new Sqrt(0.25);
+        assertEquals(0.5, sqrt.calc(), EPS);
     }
 
     @Test
     void calcFindsSqrtOfLargeNumber() {
-        Sqrt sqrt = new Sqrt(14400.0);
-        assertEquals(120.0, sqrt.calc(), EPS);
+        Sqrt sqrt = new Sqrt(10000.0);
+        assertEquals(100.0, sqrt.calc(), EPS);
     }
 }
